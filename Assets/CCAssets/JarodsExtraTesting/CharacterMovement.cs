caf73b41a21a1fe4f35a36f1975db7e113e2e80d
TheJarodSpencer <92797920+TheJarodSpencer@users.noreply.github.com>
2024-10-16 15:37:27 -0500

    Pushing idk

--- Assets/CCAssets/JarodsExtraTesting/CharacterMovement.cs
+++ Assets/CCAssets/JarodsExtraTesting/CharacterMovement.cs
@@ -1,21 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class RoundManager : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    public Text roundNumText;
-
     // Start is called before the first frame update
     void Start()
     {
         
-    }
-
-    public void NextRound(int basketNum)
-    {
-        roundNumText.text = "ROUND: " + basketNum;
     }
 
     // Update is called once per frame
