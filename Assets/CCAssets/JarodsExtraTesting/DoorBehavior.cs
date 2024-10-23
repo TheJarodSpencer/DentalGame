c5eefcdbceb22f8c1f85cb5911ad7b6f0c404a72
TheJarodSpencer <92797920+TheJarodSpencer@users.noreply.github.com>
2024-10-23 15:04:08 -0500

    the uhh character movemet push?

---
+++ Assets/CCAssets/JarodsExtraTesting/DoorBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    public Transform character;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
