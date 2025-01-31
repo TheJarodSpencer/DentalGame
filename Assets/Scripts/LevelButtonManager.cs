a494d59ef90fc391a6af563e060636272fd216f7
nimorga <nicolemorgan2002@hotmail.com>
2025-01-31 13:17:05 -0600

    Implement Button Functionality for Dia and Med

--- Assets/Scripts/LevelButtonManager.cs
+++ Assets/Scripts/LevelButtonManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class LevelButtonManager : MonoBehaviour
 {
@@ -9,6 +11,32 @@ public class LevelButtonManager : MonoBehaviour
     public GameObject medicineButton;
     public GameObject backButtonFromAxium;
     public GameObject axiumButton;
+    public GameObject diagnosisAns;
+    public GameObject medicineAns;
+    public GameObject backButtonFromDiaAndMed;
+
+    //For the Diagnosis and Medicine Anwsers
+    public Button[] medButtons;
+    public Button[] diaButtons;
+    public TextAsset diaFile; 
+    public TextAsset medFile;
+
+    void Start(){
+        LoadTextFromFile(diaFile, diaButtons);
+        LoadTextFromFile(medFile, medButtons);
+    }
+
+    void LoadTextFromFile(TextAsset textFile, Button[] buttons){
+        string[] lines = textFile.text.Split('\n');
+
+        for(int i = 0; i < buttons.Length && i < lines.Length; i++){
+            TMP_Text buttonText = buttons[i].GetComponentInChildren<TMP_Text>();
+                if (buttonText != null)
+                {
+                    buttonText.text = lines[i].Trim(); // Set button text from file
+                }
+        }
+    }
 
     public void OnClickOfAxium(){
         axiumUIPanel.SetActive(true);
@@ -30,19 +58,26 @@ public class LevelButtonManager : MonoBehaviour
         Camera.main.transform.rotation = Quaternion.Euler(5f, 0f, 0f); 
     }
 
+    public void OnClickGoBackDiaAndMed(){
+        medicineAns.SetActive(false);
+        diagnosisAns.SetActive(false);
+        backButtonFromDiaAndMed.SetActive(false);
+        diagnosisButton.SetActive(true);
+        medicineButton.SetActive(true);
+
+    }
+
     public void OnClickOfDiagnosis(){
-
+        diagnosisButton.SetActive(false);
+        medicineButton.SetActive(false);
+        diagnosisAns.SetActive(true);
+        backButtonFromDiaAndMed.SetActive(true);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnClickOfMedicine(){
+        diagnosisButton.SetActive(false);
+        medicineButton.SetActive(false);
+        medicineAns.SetActive(true);
+        backButtonFromDiaAndMed.SetActive(true);
     }
 }
