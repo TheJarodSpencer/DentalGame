2177a05c35c01dd4e9aa7d7907e414a049be263f
sopmill <156019017+sopmill@users.noreply.github.com>
2025-02-05 12:35:23 -0600

    Entering ElevatorScene in LevelSelector

---
+++ Assets/Scripts/ElevatorOpen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ElevatorOpen : MonoBehaviour
+{
+    private bool playerInRange = false;
+
+    void OnTriggerEnter(Collider cube)
+    {
+        if (cube.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Debug.Log("Player entered the trigger zone");
+        }
+    }
+
+    void OnTriggerExit(Collider cube)
+    {
+        if (cube.CompareTag("Player"))
+        {
+            playerInRange = false;
+            Debug.Log("Player exited the trigger zone");
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed. Loading scene...");
+            SceneManager.LoadScene("ElevatorScene");
+        }
+    }
+}
