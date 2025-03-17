7ad10d7243d4b4aeed448a0f5e22f8fc507a2ec1
sopmill <156019017+sopmill@users.noreply.github.com>
2025-03-16 21:27:19 -0500

    Fixed Button

--- Assets/Scripts/PauseButton.cs
+++ Assets/Scripts/PauseButton.cs
@@ -7,6 +7,7 @@ public class PauseButton : MonoBehaviour
 {
     // Start is called before the first frame update
     public void OnPauseButton(){
+        Debug.Log("Pause Button Clicked");
         SceneManager.LoadScene("SettingsScene");
     }
 }
