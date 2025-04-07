01cf322356325c182de9a2975a119b0ff79b3153
sopmill <156019017+sopmill@users.noreply.github.com>
2025-04-06 23:14:20 -0500

    adding rough help

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
 public class SettingsMenu : MonoBehaviour
 {
+    public GameObject HelpPanel;
     public void OnPlayButton(){
         SceneManager.LoadScene("LevelSelectorMainFloor");
     }
     public void OnReturnToStartButton(){
         SceneManager.LoadScene("WelcomeScene");
     }
+    public void OnHelpButton(){
+        HelpPanel.SetActive(true);
+    }
+
 }
